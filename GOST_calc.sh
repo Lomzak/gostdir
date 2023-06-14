bb82cbc56d55eb794158b9fed94a68fb384e15a1
Lomzak <LomzakV@gmail.com>
2023-06-14 17:22:41 +0300

    Change file list from txt file to array

--- GOST_calc.sh
+++ GOST_calc.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 dir_for_report="Etalon"
 if test -d $dir_for_report;then rm -r $dir_for_report;mkdir $dir_for_report;else mkdir $dir_for_report;fi
-if test -f filelist.txt;then rm filelist.txt;fi
 
-find . -type f -print | sort >> temp.txt
-filelist="filelist.txt";temp="temp.txt"
-sed -e '/temp.txt/d' -e '/GOST_calc.sh/d' $temp >> $filelist
-rm $temp
+#if test -f filelist.txt;then rm filelist.txt;fi
+declare -a file_list
+readarray file_list < <(find . -type f -print)
 
 result_2012_512="GOST_34.11-2012(512).txt"
 result_2012_256="GOST_34.11-2012(256).txt"
@@ -16,15 +14,13 @@ echo -e "------------------------------------------------------GOST 34.11-2012(5
 echo -e "------------------------------------------------------GOST 34.11-2012(256)------------------------------------------------------\n" >> $result_2012_256
 echo -e "----------------------------------------------------------GOST 34.11-94---------------------------------------------------------\n" >> $result_1994_256
 
-for var in $(cat $filelist)
+for var in "${!file_list[@]}"
 do
-sudo gostsum --gost-2012-512    $var >> $result_2012_512
-sudo gostsum --gost-2012        $var >> $result_2012_256
-sudo gostsum --gost-94          $var >> $result_1994_256
+sudo gostsum --gost-2012-512    ${file_list[$var]} >> $result_2012_512
+sudo gostsum --gost-2012        ${file_list[$var]} >> $result_2012_256
+sudo gostsum --gost-94          ${file_list[$var]} >> $result_1994_256
 echo -e "------------------------------------------------------------------------------------------------------------------------------------\n" | tee -a $result_2012_512 $result_2012_256 $result_1994_256 > /dev/null
 done
-
-rm $filelist
 
 echo -e "\n===================================================================================================================================" | tee -a $result_2012_512 $result_2012_256 $result_1994_256 > /dev/null
 mv $result_2012_256 $result_2012_512 $result_1994_256 $dir_for_report
